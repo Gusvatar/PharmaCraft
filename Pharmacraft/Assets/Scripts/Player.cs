96aab525be7f23bd3a6c9223d838e240f7e863b8
Gustavo Herbster <gustavoherbstermacedo@gmail.com>
2023-10-23 23:15:15 -0300

    Primeiro commit no Git
    Primeira versão do jogo para o repositório

---
+++ Pharmacraft/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+   
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = direction.normalized * Speed;
+
+        if(direction.x != 0)
+        {
+            ResetLayer();
+            anim.SetLayerWeight(2, 1);
+
+            if(direction.x > 0)
+            {
+                sprite.flipX = false;
+            } else if(direction.x < 0)
+            {
+                sprite.flipX = true;
+            }
+        }
+
+        if(direction.y > 0 && direction.x == 0){
+            ResetLayer();
+            anim.SetLayerWeight(1, 1);
+        }
+
+        if(direction.y < 0 && direction.x == 0){
+            ResetLayer();
+            anim.SetLayerWeight(0, 1);
+        }
+
+        if(direction != Vector2.zero)
+        {
+            anim.SetBool("walking", true);
+        }else
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+
+    private void ResetLayer()
+    {
+        anim.SetLayerWeight(0, 0);
+        anim.SetLayerWeight(1, 0);
+        anim.SetLayerWeight(2, 0);
+    }
+}
