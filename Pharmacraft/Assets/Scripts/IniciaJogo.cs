04392a4af0328ee80d22a7249741194fc93b3619
Gustavo Herbster <gustavoherbstermacedo@gmail.com>
2023-12-09 18:14:55 -0300

    Audio no botao do menu

--- Pharmacraft/Assets/Scripts/IniciaJogo.cs
+++ Pharmacraft/Assets/Scripts/IniciaJogo.cs
@@ -5,7 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class IniciaJogo : MonoBehaviour
 {
+    public AudioSource Audio;
+    public AudioClip ClickBotao;
     public void iniciaJogo() {
+        Audio.clip = ClickBotao;
+        Audio.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
